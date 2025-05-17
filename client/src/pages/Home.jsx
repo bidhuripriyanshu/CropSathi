6982ddbcad102e3a247efe716c20e19d818d9fb5
bidhuripriyanshu <bidhuripriyanshu351@gmail.com>
2025-05-17 21:54:40 +0530

    Initial commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import Cookies from 'js-cookie';
+import axios from "axios";
+import { ToastContainer } from "react-toastify";
+import NewCropCard from '../components/NewCropCard'
+import url from '../url';
+import getCropDetails from "../util/CropDetails";
+import { useTranslation } from 'react-i18next';
+
+const Home = () => {
+  const navigate = useNavigate();
+  const [cookies, , removeCookie] = useCookies();
+  const [username, setUsername] = useState("");
+  const [id, setId] = useState('');
+  const { t } = useTranslation();
+  const [crops, setCrops] = useState([]);
+
+  useEffect(() => {
+    const verifyCookie = async () => {
+      if (!cookies.token) {
+        navigate("/login");
+        return;
+      }
+      try {
+        const { data } = await axios.post(
+          `${url}`,
+          { tok: cookies.token },
+          { withCredentials: true }
+        );
+        const { status, user, id, language } = data;
+        setUsername(user);
+        setId(id);
+        Cookies.set('id', id);
+        Cookies.set('language', language);
+        Cookies.set('username', user);
+        if (!status) {
+          removeCookie("token");
+          Cookies.remove('id');
+          navigate("/login");
+        }
+      } catch (error) {
+        removeCookie("token");
+        Cookies.remove('id');
+        navigate("/login");
+      }
+    };
+    verifyCookie();
+  }, [cookies, navigate, removeCookie]);
+
+  useEffect(() => {
+    if (id) {
+      axios.post(`${url}/Cropfetch`, { id })
+        .then(response => {
+          const { Crop1, Crop2, Crop3, Crop4, Crop5 } = response.data;
+          const cropNames = [Crop1, Crop2, Crop3, Crop4, Crop5];
+          const cropDetailsArray = cropNames.map(getCropDetails);
+          setCrops(cropDetailsArray);
+        })
+        .catch(error => {
+          console.error('Error fetching crops:', error);
+        });
+    }
+  }, [id]);
+
+  return (
+    <>
+      <div className="home_page">
+        <div className="new_card">
+          {crops.length > 0 && <NewCropCard crop={crops[0]} crops={crops} />}
+        </div>
+        <div className="card_container">
+          {/* Add TopCropCard and RestCropCards here if needed */}
+        </div>
+      </div>
+      <ToastContainer />
+    </>
+  );
+};
+
+
+export default Home;
---
+++ server/index.js
@@ -0,0 +1,46 @@
+const { connectDB } = require("./config/db"); //connectDB is a function that connects to the database
+const express = require("express"); //express is a framework for building web applications
+const cors = require("cors"); //cors is a middleware that allows cross-origin requests
+require("dotenv").config(); //dotenv is a module that loads environment variables from a .env file into process.env
+const cookieParser = require("cookie-parser"); //cookieParser is a middleware that parses cookies
+const authRoute = require("./Routes/AuthRoute"); //authRoute is a route that handles authentication
+const dataRoute = require("./Routes/DataRoute"); //dataRoute is a route that handles data
+const cropRoute = require("./Routes/CropRoute"); //cropRoute is a route that handles crops
+const postRoute = require("./Routes/PostRoute"); //postRoute is a route that handles posts
+const commentRoute = require("./Routes/CommentRoute"); //commentRoute is a route that handles comments
+const { PORT } = process.env; //PORT is the port number that the server will listen on
+
+
+const app = express(); //create an express application
+
+connectDB(); //connect to the database
+console.log();
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+app.use(
+  cors({
+    //CORS (Cross origin resource sharing): You can allow requests from other domains to access the resources on your server by using the cors() express middleware function. 
+    origin: ["http://localhost:4000", "http://localhost:5173", "https://cropmate.onrender.com", "https://crop-mate-33nf5j7jj-sanjus-projects.vercel.app" , "https://crop-mate.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+
+app.use(express.json());  
+//express.json(): The express.json() will add a body property to the request or req object. This includes the request body's parsed JSON data. req.body in your route handler function will allow you to access this data
+
+app.use("/", authRoute);
+app.use("/", dataRoute);
+app.use("/", cropRoute);
+app.use("/", postRoute);
+app.use("/", commentRoute);
+
+
+// Adding by priyanshu
+app.get("/", (req, res) => {
+  res.send("Hello World");
+}); 
